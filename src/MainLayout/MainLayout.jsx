d712dc1c0ee3f7ae83fbbcdd5231a0ffd5a76602
jahidhasan6676 <jh18186676@gmail.com>
2024-11-18 12:41:52 +0600

    added the Vocabulary Learning website react route setup

---
+++ src/MainLayout/MainLayout.jsx
@@ -0,0 +1,12 @@
+
+
+const MainLayout = () => {
+    return (
+        <div>
+            <h2 className="text-2xl">Main layout</h2>
+           
+        </div>
+    );
+};
+
+export default MainLayout;
